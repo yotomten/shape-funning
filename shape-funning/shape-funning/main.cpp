226f7f21112d7a0d39f09fd690f9cee42165f426
magnuzo <hajk_92@hotmail.com>
2016-11-02 11:44:10 +0100

    Glfw, OpenGL and Glew set up properly for the project. Hello Window from learnopengl finished.

---
+++ shape-funning/shape-funning/main.cpp
@@ -0,0 +1,106 @@
+
+#include <iostream>
+
+//GlEW, before GLFW
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+// Should make it cross platform
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
+
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+	// Init GLFW and set all its required options
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Use OpenGl 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Don't use old functionality
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Necessary for Mac OSX
+
+	// Init GLFWwindow object
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLEW to setup function pointers for OpenGl
+
+	glewExperimental = GL_TRUE; // Use core profile modern OpenGL techniques
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to init GLEW" << std::endl;
+		return -1;
+	}
+
+	// Define width and height for OpenGL (viewport dimensions)
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height); // Framebuffer size in pixels instead of screen coordinates
+	glViewport(0, 0, width, height); // (0, 0) = Southwest corner of window
+	// OpenGL uses this data to map from processed coordinates in range (-1,1)
+	// to (0,800) and (0,600)
+
+	glfwSetKeyCallback(window, key_callback);
+
+	// Triangle vertices
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// Init triangle VBO
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Compile shader
+	GLuint vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents(); // Check if events have been activated
+
+		// Rendering commands
+		glClearColor(0.2f, 0.5f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate(); // Clear allocated resources
+	return 0;
+}
